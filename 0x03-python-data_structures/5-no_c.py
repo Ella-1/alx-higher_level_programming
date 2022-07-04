9aeedd757c902fc874da059b7481f5694a5ec7f8
Bata <es9897288@.com>
2022-07-04 13:00:33 -0700

    mandatory task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,8 @@
-#!/usr/bin/python
-
-
+#!/usr/bin/python3
 def no_c(my_string):
-    string_list = list(my_string)
-    for i in range(len(string_list)):
-        if string_list[i] == 'c' or string_list[i] == 'C':
-            string_list[i] = ""
-    my_string = "".join(string_list)
-    return my_string
+    new_str = ''
+    for i in my_string:
+        if i != 'C' and i != 'c':
+            new_str += i
+
+    return new_str
