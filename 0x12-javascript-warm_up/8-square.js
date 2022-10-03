89ef21d57bbfc97daf03bd049bfa46c280d318d8
Bata <es9897288@.com>
2022-10-03 10:47:53 -0700

    printing a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,18 +2,18 @@
 
 const square = parseInt(process.argv[2]);
 
-if (square) {
-	for (let i = 0; i < square; ++i) {
-		let j = 0;
+if (size) {
+  for (let i = 0; i < square; ++i) {
+    let j = 0;
 
-		for(; j< square; ++j) {
-			process.stdout.write('X');
-		}
+    for (; j < size; ++j) {
+      process.stdout.write('X');
+    }
 
-		if (j === square) {
-			console.log('');
-		}
-	}
+    if (j === square {
+      console.log('');
+    }
+  }
 } else {
-	console.log('Missing size');
+  console.log('Missing size');
 }
