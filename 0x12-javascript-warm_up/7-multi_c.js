08944349a1b0d7dd7fb16e4fa748ed5115baa3a5
Bata <es9897288@.com>
2022-10-03 10:35:12 -0700

    looping through a file entered in a terminal

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const limits = parseInt(process.argv[2]);
+
+if (limits) {
+	for (let i = 0; i< limits; ++i) {
+		console.log('C is fun');
+	}
+} else {
+	console.log('Missing number of occurrences');
+}
