13c453b690e908f4c422e9d47b9cee90362841fc
Bata <es9897288@.com>
2022-10-03 10:16:12 -0700

    script that prints my number + argv word

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const arg = parseInt(process.argv[2]);
+const argm = parseInt(process.argv[2]);
 
-if (arg) {
-	console.log('My number: ' + arg);
+if (argm) {
+  console.log('My number: ' + argm);
 } else {
-	console.log('Not a number');
+  console.log('Not a number');
 }
