8ae06fd7788c27d24d5f6172c393ea76d328cdfb
Bata <es9897288@.com>
2022-10-05 10:43:19 -0700

    inheritance to a square

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,7 +1,35 @@
 #!/usr/bin/node
 
-module.exports = class Square extends requirs('./4-rectangle.js') {
-	constructor (size) {
-		super(size,size);
-	}
+// - Rotate and Double the width and height of the Rectangle
+
+module.exports = class Rectangle {
+  constructor (width, height) {
+    if (typeof width === 'number' && typeof height === 'number' && width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  print (char = 'X') {
+    for (let i = 0; i < this.height; ++i) {
+      let j = 0;
+
+      for (; j < this.width; ++j) {
+        process.stdout.write(char);
+      }
+
+      if (j === this.width) {
+        console.log('');
+      }
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 };
