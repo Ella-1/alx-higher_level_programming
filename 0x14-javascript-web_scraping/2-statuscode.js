0de17019e936eece2405e5d79f640b7f12835eaf
Ella-1 <es9897288@gmail.com>
2022-11-15 04:12:38 -0800

    script that diaplay status code of get request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const request = requirre('request');
+const request = require('request');
 
-request(process.argv[2], function(_err, res) {
-  cosole.log('code:', res.statusCode); // prints the responce status code
+request(process.argv[2], function (_err, res) {
+  console.log('code:', res.statusCode); // Print the response status code if a response was received
 });
